d1588d2c8cbd509f85eae839204ffcb51014db44
Matthew Willig <mw1153@rit.edu>
2018-03-26 17:51:16 -0400

    Location Changes
    Added a file that was being called in init_util.sh
    changed init_util.sh so that it pulls from new repository

--- bash/init_util.sh
+++ bash/init_util.sh
@@ -3,10 +3,14 @@ sudo apt-get -y update
 sudo apt-get install -y openssh-server > /dev/null
 sudo apt-get install -y apache2 > /dev/null
 sudo apt-get install -y git > /dev/null
-git clone https://github.com/98Giraffe/RIT_Capstone_2016.git
-sudo cp RIT_Capstone_2016/tor/deploy.sh /var/www/html/
-sudo cp RIT_Capstone_2016/tor/config/torrc.da /var/www/html/
-sudo cp RIT_Capstone_2016/tor/update_torrc_DAs.sh /var/www/html/
+#git clone https://github.com/98Giraffe/RIT_Capstone_2016.git
+git clone https://github.com/arg5173/RIT-Tor-Capstone-Spring2018.git
+#sudo cp RIT_Capstone_2016/tor/deploy.sh /var/www/html/
+#sudo cp RIT_Capstone_2016/tor/config/torrc.da /var/www/html/
+#sudo cp RIT_Capstone_2016/tor/update_torrc_DAs.sh /var/www/html/
+sudo cp RIT-Tor-Capstone-Spring2018/bash/deploy.sh
+sudo cp RIT-Tor-Capstone-Spring2018/Tor/config/torrc.da /var/www/html/
+sudo cp RIT-Tor-Capstone-Spring2018/bash/update_torrc_DAs.sh /var/www/html/
 sudo useradd -m -d /home/tor tor
 sudo echo tor:wordpass | sudo chpasswd tor
 sudo usermod -aG sudo tor
