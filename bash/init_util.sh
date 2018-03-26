634bde7a1f56d5b4803fd9267aad4741e62c092a
Matthew Willig <mw1153@rit.edu>
2018-03-26 18:07:04 -0400

    Added info removed old lines

--- bash/init_util.sh
+++ bash/init_util.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+#
+# Prereques:    None
+#
+# Dependencies: RIT-Tor-Capstone-Spring2018/bash/deploy.sh
+#               RIT-Tor-Capstone-Spring2018/Tor/config/torc.da
+#               RIT-Tor-Capstone-Spring2018/bash/update_torrc_DAs.sh
+#########
+
+
 sudo apt-get -y update
 sudo apt-get install -y openssh-server > /dev/null
 sudo apt-get install -y apache2 > /dev/null
 sudo apt-get install -y git > /dev/null
-#git clone https://github.com/98Giraffe/RIT_Capstone_2016.git
 git clone https://github.com/arg5173/RIT-Tor-Capstone-Spring2018.git
-#sudo cp RIT_Capstone_2016/tor/deploy.sh /var/www/html/
-#sudo cp RIT_Capstone_2016/tor/config/torrc.da /var/www/html/
-#sudo cp RIT_Capstone_2016/tor/update_torrc_DAs.sh /var/www/html/
 sudo cp RIT-Tor-Capstone-Spring2018/bash/deploy.sh /var/www/html/
 sudo cp RIT-Tor-Capstone-Spring2018/Tor/config/torrc.da /var/www/html/
 sudo cp RIT-Tor-Capstone-Spring2018/bash/update_torrc_DAs.sh /var/www/html/
