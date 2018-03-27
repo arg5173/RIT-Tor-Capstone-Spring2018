b9cbb030f20fbdfa6a47a286b0b1cd5a1e357ee4
Andrew <arg5173@rit.edu>
2018-03-27 16:20:10 -0400

    Removed auto-install of openssl and wget, added them to prereqs. Added prompt for key-generation. Terraform apply require user to accept.

---
+++ terraform/automate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Authors: 
+#
+# Prerequisites: openssl for keygen
+# 				terraform.exe in local directory 
+#				AWS Access and Secret Key need to be in terraform.tfvars
+#	
+#########
+
+# Generate key pair
+read -p "Generate new key pair? :" yn
+case $yn in
+	[Yy]* ) ssh-keygen -f tor-key -b 4092 -t rsa -q -N ""; PUB_KEY=$(cat tor-key.pub);;
+    [Nn]* ) echo "no key generated" 
+esac
+
+# Grab Terraform
+wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
+# Extract it
+unzip terraform_0.11.3_linux_amd64.zip
+rm terraform_0.11.3_linux_amd64.zip
+
+# Add our key to the main Terraform configuration file
+sed -i -e 's|public_key = \"\"|public_key = '\""$PUB_KEY"\"'|' aws.tf
+
+# Add the private ssh key to the authorized keys
+echo "$PUB_KEY" >> ~/.ssh/authorized_keys
+
+# Download the terraform aws module
+./terraform init
+
+# Plan and prepare to apply
+# User must enter "yes" for apply to go through
+./terraform apply
