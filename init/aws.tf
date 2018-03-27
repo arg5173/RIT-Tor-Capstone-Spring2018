5c7d5cbddbdbfd58be97b66856ccee2a835e66c7
Andrew <arg5173@rit.edu>
2018-03-27 13:53:49 -0400

    added other required instances (DAs, exit node, client, hidden service) for initial testing

--- init/aws.tf
+++ init/aws.tf
@@ -77,6 +77,152 @@ resource "aws_instance" "relay" {
     }
 }
 
+resource "aws_instance" "client" {
+    ami = "ami-628ad918"  # Debain strech - 64 bit
+    instance_type = "t2.micro"
+    key_name = "${aws_key_pair.tor-key.key_name}"
+    
+    tags {
+        Name = "Tor Client"
+    }
+
+    provisioner "file" {
+        source = "tor-key"
+        destination = "/home/admin/tor-key"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "sudo apt-get install wget -y",
+          "wget https://raw.githubusercontent.com/arg5173/RIT-Tor-Capstone-Spring2018/master/bash/deploy.sh",
+          "chmod +x deploy.sh",
+          "chmod 400 tor-key",
+          "sudo ./deploy.sh CLIENT ${aws_instance.utility.public_ip}"
+        ]
+    }
+	
+	timeouts {
+		create = "60m"
+		delete = "30m"
+	}
+
+    connection {
+        type = "ssh"
+        user = "admin"
+        private_key = "${file("tor-key")}"
+    }
+}
+
+resource "aws_instance" "hiddenservice" {
+    ami = "ami-628ad918"  # Debain strech - 64 bit
+    instance_type = "t2.micro"
+    key_name = "${aws_key_pair.tor-key.key_name}"
+    
+    tags {
+        Name = "Hidden Service"
+    }
+
+    provisioner "file" {
+        source = "tor-key"
+        destination = "/home/admin/tor-key"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "sudo apt-get install wget -y",
+          "wget https://raw.githubusercontent.com/arg5173/RIT-Tor-Capstone-Spring2018/master/bash/deploy.sh",
+          "chmod +x deploy.sh",
+          "chmod 400 tor-key",
+          "sudo ./deploy.sh HS ${aws_instance.utility.public_ip}"
+        ]
+    }
+	
+	timeouts {
+		create = "60m"
+		delete = "30m"
+	}
+
+    connection {
+        type = "ssh"
+        user = "admin"
+        private_key = "${file("tor-key")}"
+    }
+}
+
+resource "aws_instance" "exit" {
+    ami = "ami-628ad918"  # Debain strech - 64 bit
+    instance_type = "t2.micro"
+    key_name = "${aws_key_pair.tor-key.key_name}"
+    
+    tags {
+        Name = "Tor Exit Node"
+    }
+
+    provisioner "file" {
+        source = "tor-key"
+        destination = "/home/admin/tor-key"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "sudo apt-get install wget -y",
+          "wget https://raw.githubusercontent.com/arg5173/RIT-Tor-Capstone-Spring2018/master/bash/deploy.sh",
+          "chmod +x deploy.sh",
+          "chmod 400 tor-key",
+          "sudo ./deploy.sh EXIT ${aws_instance.utility.public_ip}"
+        ]
+    }
+	
+	timeouts {
+		create = "60m"
+		delete = "30m"
+		delete = "30m"
+	}
+
+    connection {
+        type = "ssh"
+        user = "admin"
+        private_key = "${file("tor-key")}"
+    }
+}
+
+resource "aws_instance" "da" {
+    ami = "ami-628ad918"  # Debain strech - 64 bit
+    instance_type = "t2.micro"
+    key_name = "${aws_key_pair.tor-key.key_name}"
+    
+    tags {
+        Name = "Tor Directory Authority"
+    }
+
+    provisioner "file" {
+        source = "tor-key"
+        destination = "/home/admin/tor-key"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "sudo apt-get install wget -y",
+          "wget https://raw.githubusercontent.com/arg5173/RIT-Tor-Capstone-Spring2018/master/bash/deploy.sh",
+          "chmod +x deploy.sh",
+          "chmod 400 tor-key",
+          "sudo ./deploy.sh DA ${aws_instance.utility.public_ip}"
+        ]
+    }
+	
+	timeouts {
+		create = "60m"
+		delete = "30m"
+	}
+
+    connection {
+        type = "ssh"
+        user = "admin"
+        private_key = "${file("tor-key")}"
+    }
+}
+
+
 resource "aws_key_pair" "tor-key" {
     key_name = "tor-key"
     public_key = ""
