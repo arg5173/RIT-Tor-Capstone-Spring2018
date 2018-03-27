b9a8990231b5b9e6acbc2f7b53d9e29e80cfe2e1
Andrew Gunkle <arg5173@rit.edu>
2018-03-27 00:51:41 -0400

    Updated docstrings and comments, changed Tor to match tor directory in repository

--- bash/init_util.sh
+++ bash/init_util.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 #
-# Prereques:    None
+# Private TOR Network Project
+# Utility Server Initialization Script
+#
+# Prerequisites: None
 #
 # Dependencies: RIT-Tor-Capstone-Spring2018/bash/deploy.sh
 #               RIT-Tor-Capstone-Spring2018/Tor/config/torc.da
 #               RIT-Tor-Capstone-Spring2018/bash/update_torrc_DAs.sh
 #########
 
-
+# Update and install necessary programs
 sudo apt-get -y update
 sudo apt-get install -y openssh-server > /dev/null
 sudo apt-get install -y apache2 > /dev/null
 sudo apt-get install -y git > /dev/null
+
+# Clone from repository and grab needed files
 git clone https://github.com/arg5173/RIT-Tor-Capstone-Spring2018.git
 sudo cp RIT-Tor-Capstone-Spring2018/bash/deploy.sh /var/www/html/
-sudo cp RIT-Tor-Capstone-Spring2018/Tor/config/torrc.da /var/www/html/
+sudo cp RIT-Tor-Capstone-Spring2018/tor/config/torrc.da /var/www/html/
 sudo cp RIT-Tor-Capstone-Spring2018/bash/update_torrc_DAs.sh /var/www/html/
+
 sudo useradd -m -d /home/tor tor
 sudo echo tor:wordpass | sudo chpasswd tor
 sudo usermod -aG sudo tor
